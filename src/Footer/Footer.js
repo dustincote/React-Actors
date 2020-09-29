e6ef39ea67880ee2548f188216654c844365c22f
Dustin Cote <dustincote@hotmail.com>
2020-09-29 12:05:37 -0500

    base completed

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer><h3>© React Actors </h3></footer>
+        );
+    }
+}
+
+export default Footer;
